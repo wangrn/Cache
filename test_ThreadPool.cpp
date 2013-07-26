3b4b614d7976501ebd2a8fac929f895435928b18
wangrn <wang_rn@163.com>
2013-07-26 16:34:49 +0800

    Create test_ThreadPool.cpp

---
+++ test_ThreadPool.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "thread_pool.h"
+
+class thread_runner_test : public thread_pool::thread_runner
+{
+public:
+    virtual ~thread_runner_test() {}
+    virtual void run() {
+        printf("trt: %d\n", isn);
+    }
+    int isn;
+};
+
+int main(int argc, char* argv[]) {
+    thread_pool* tp = new thread_pool();
+    tp->start(10);
+    for (int i = 0; i < 100; ++i) {
+        thread_runner_test* trt = new thread_runner_test;
+  trt->isn = i;
+        tp->add_task(trt);
+    }
+
+    int recv_count = 0;
+
+    while (1) {
+        thread_runner_test* trt = (thread_runner_test*)tp->get_task();
+	if (trt) {
+	    recv_count += 1;
+	    if (recv_count == 100)
+	        break;
+	    else
+	        continue;
+	}
+        usleep(1000);
+    }
+
+    printf ("recv_count:%d\n", recv_count);
+    return 0;
+}
