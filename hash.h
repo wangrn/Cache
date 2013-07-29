6962e239d051e1624707d84b60614e949efee8fe
wangrn <wang_rn@163.com>
2013-07-29 11:26:36 +0800

    Update hash.h

--- hash.h
+++ hash.h
@@ -10,11 +10,231 @@ using namespace std;
 
 class hash
 {
+public:
+    class knot
+    {
+    public:
+        unsigned int key;
+        void *arg;
+        knot *next;
+        
+        knot()
+        {
+            key = 0;
+            arg = NULL;
+            next = NULL;
+        }
+    };
+    
+    knot *free_list;
+    knot **table;
+    knot *pool;
+    vector<knot*> second_pools;
+    unsigned int pool_size;
+    unsigned int second_pool_size;
+    int key_count;
+    
+    unsinged int mf;  // mod factor
+    unsigned int table_size;
+    
+    hash()
+    {
+        free_list = 0;
+        table = 0;
+        pool = 0;
+        mf = 0;
+        table_size = 0;
+        second_pool_size = 0;
+        key_count = 0;
+    }
+    
+    knot* alloc_base_pool(unsigned int size)
+    {
+        pool = new knot[size];
+        clear_one_pool(pool, size);
+        return pool;
+    }
+    
+    void init(unsigned int _pool_size, unsigned int _second_pool_size, unsigned int _table_size, unsigned int _mf)
+    {
+        free_list = alloc_base_pool(_pool_size);
+        
+        table = (knot**) malloc(sizeof(knot*)*_table_size);
+        memset(table, 0, sizeof(knot*)*_table_size);
+        
+        mf = _mf;
+        table_size = _table_size;
+        pool_size = _pool_size;
+        second_pool_size = _second_pool_size;
+    }
+    
+    void add_second_pool() 
+    {
+        knot *p = new knot[second_pool_size];
+        knot* last = clear_one_pool(p, second_pool_size);
+        
+        last->next = free_list;
+        free_list = p;
+        second_pools.push_back(p);
+    }
+    
+    knot *clear_one_pool(knot *p, unsigned int size)
+    {
+        for (int i = 0; i < size-1, i ++)
+            p[i].next = &p[i+1];
+        p[size-1].next = NULL;
+        return &p[size-1];
+    }
+    
+    void clear() 
+    {
+        if (0 == key_count) return;
+        
+        memset(table, 0, sizeof(knot*)*table_size);
+        free_list = 0;
+        for (unsigned int i = 0; i < second_pools.size(); i ++)
+        {
+            knot *p = second_pools[i];
+            knot *last = clear_one_pool(p, second_pool_size);
+            
+            last->next = free_list;
+            free_list = p;
+        }
+        knot *last = clear_one_pool(pool, pool_size);
+        last->next = free_list;
+        free_list = pool;
+        
+        key_count = 0;
+    }
+    
+    inline knot* get_free_knot()
+    {
+        knot *ret = free_list;
+        if (free_list)
+        {
+            free_list = ret->next;
+        }
+        if (ret)
+        {
+            ret->key = 0;
+            ret->arg = 0;
+            ret->next = 0;
+            key_count ++;
+        }
+        return ret;
+    }
+    
+    inline void collect_free_knot(knot *k)
+    {
+        key_count --;
+        assert(key_count >= 0);
+        k->arg = 0;
+        k->key = 0;
+        k->next = free_list;
+        free_list = k;
+    }
+    
+    inline knot* find(unsinged int key)
+    {
+        unsigned int mk = key % mf;
+        knot *n = table[mk];
+        while (n && n->key != key)
+            n = n->next;
+        return n;
+    }
+    inline void* find2(unsigned int key)
+    {
+        knot* ret = find(key);
+        if (ret) return ret->arg;
+        return NULL;
+    }
+    
+    knot* insert(unsigned int key, void *arg, bool &exist)
+    {
+        exist = false;
+        knot *k = find(key);
+        if (k) 
+        {
+            exist = true;
+            return k;
+        }
+        else
+        {
+            while ((k = get_free_knot) == NULL)
+                add_second_pool();
+            
+            k->key = key;
+            k->arg = arg;
+            unsigned int mk = key %mf;
+            k->next = table[mk];
+            table[mk] = k;
+            return k;
+        }
+    }
+    
+    void remove(unsigned int key)
+    {
+        unsigned int mk = key % mf;
+        knot *n = table[mk];
+        if (!n) return;
+        if (n->key = key)
+        {
+            table[mk] = n->next;
+            collect_free_knot(n);
+            return ;
+        }
+        
+        while(n->next && n->next->key != key)
+            n = n->next;
+        
+        if (n && n->next && n->next->key == key)
+        {
+            knot *tmp = n->next;
+            n->next = n->next->next;
+            collect_free_knot(tmp);
+        }
+    }
+    
+    void collect_all_knot(vector<knot*> &cv)
+    {
+        for (int i = 0; i < table_size; i ++)
+        {
+            knot *k = table[i];
+            while (k)
+            {
+                cv.push_back(k);
+                k = k->next;
+            }
+        }
+    }
+    
+    void finit()
+    {
+        if (pool == 0) return;
+        delete [] pool;
+        free_list = 0;
+        table = 0;
+        pool = 0;
+        for (int i = 0; i < second_pools.size(); i ++)
+        {
+            knot *n = second_pools[n];
+            delete[] n;
+        }
+        second_pools.clear();
+        key_count = 0;
+    }
+    
+    void dump() 
+    {
+        //TODO
+    }
 };
 
 
+#define HASH_TABLE_FIRST_MOD 1997
 class hash_table
 {
+    
 };
 
 #endif
