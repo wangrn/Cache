641b03a026e60dbbbc07577963cd983a83815fd7
wangrn <wang_rn@163.com>
2013-07-29 10:56:29 +0800

    Create hash.h

---
+++ hash.h
@@ -0,0 +1,20 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <assert.h>
+using namespace std;
+
+class hash
+{
+};
+
+
+class hash_table
+{
+};
+
+#endif
