c3705cf42f144a51e96d3bf9c8b748703579506d
wangrn <wang_rn@163.com>
2013-07-29 10:44:59 +0800

    Update protocal.h

--- protocal.h
+++ protocal.h
@@ -2,4 +2,62 @@
 #define CACHE_PROTOCAL_H
 
 
+enum Command
+{
+    CMD_CACHE_UPDATE_STATE_UPDATING = 1,
+    CMD_CACHE_UPDATE_STATE_ESTABLISHED = 2,
+    CMD_CACHE_UPDATE_STATE_APPENDING = 3,
+    CMD_CACHE_GET_STATUE = 4,
+    
+    CMD_CACHE_ADD  = 101,
+    CMD_CACHE_ADD_BATCH ,
+    CMD_CACHE_GET ,
+    CMD_CACHE_GET_BATCH ,
+    
+    CMD_CACHE_SHUTDOWN = 1001,
+};
+
+enum Error
+{
+    ERR_EMPTY_REQUEST = 10001,
+    ERR_TOOLARGE_REQUEST ,
+    ERR_INVALID_REQUEST_SIZE ,
+    ERR_FORBID_BY_CUR_STATE ,
+    ERR_OOM ,
+    ERR_TIMEOUT,
+    ERR_UNKNOWN_CMD ,
+};
+
+const int LIMIT_REQUEST_SIZE = 64*1024*1024;
+
+
+class request_header
+{
+public:
+    unsigned int length;
+    Command cmd;
+    
+    bool encode(char *buf, unsigned int size, unsigned int& offset)
+    {
+        if (offset + sizoef(request_header) > size)
+            return false;
+        request_header *rh = (request_header*)(buf + offset);
+        rh->length = length;
+        rh->cmd = cmd;
+        offset += sizeof(request_header);
+        return true;
+    }
+    
+    bool decode(char *buf, unsigned int size, unsigned int& offset)
+    {
+        if (offset + sizoef(request_header) > size)
+            return false;
+        request_header *rh = (request_header*)(buf + offset);
+        this.length = rh->length;
+        this.cmd = rh->cmd;
+        offset += sizeof(request_header);
+        return true;
+    }
+};
+
 #endif
